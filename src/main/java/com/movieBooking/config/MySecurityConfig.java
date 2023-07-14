477d2f357a9fdf1d98add5b556bafc69d8d7d84f
Srujan Jilla <61146300+SRUJAN-JILLA@users.noreply.github.com>
2023-07-14 21:02:44 +0530

    cors error

--- src/main/java/com/movieBooking/config/MySecurityConfig.java
+++ src/main/java/com/movieBooking/config/MySecurityConfig.java
@@ -1,5 +1,7 @@
 package com.movieBooking.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.movieBooking.service.impl.UserDetailsServiceImpl;
@@ -30,17 +33,32 @@ public class MySecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-		http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable())
+		http.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers("/api/v1/**", "/generate-token/**", "/current-user/**")
 						.permitAll().anyRequest().authenticated())
 				.exceptionHandling(ex -> ex.authenticationEntryPoint(point))
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+	    http.cors().configurationSource(request -> new CorsConfiguration(corsConfiguration()));
 
 		http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
 	}
+	
+	@Bean
+    public CorsConfiguration corsConfiguration() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("https://moviebookingsrujan.azurewebsites.net/"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control, Allow-Origin", "Content-Type",
+                "Accept", "Authorization", "Origin, Accept", "X-Requested-With", "Access-Control-Request-Method",
+                "Access-Control-Request-Header"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+                "Access-Control-Request-Allow-Origin", "Access-Control-Allow-Credentials"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        return corsConfiguration;
+    }
 
 	@Bean
 	public DaoAuthenticationProvider getDaoAuthenticationProvider() {
